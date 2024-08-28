d12853cbf3c576f56c1917ed17eec5d59d3a0496
siddharth <notcodesid@gmail.com>
2024-08-28 13:04:07 +0530

    create the blog

--- server/src/router/blogRouter.ts
+++ server/src/router/blogRouter.ts
@@ -1,4 +1,8 @@
 import { Hono } from 'hono';
+import { createBlogInput } from '../../models';
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+
 
 export const blogRouter = new Hono<{
     Bindings: {
@@ -6,7 +10,37 @@ export const blogRouter = new Hono<{
     }
 }>();
 
-blogRouter.post('/', (c) => {
-  return c.text('done');
-});
+blogRouter.post('/', async (c) => {
+    const body = await c.req.json();
+    const { success, data } = createBlogInput.safeParse(body);
+  
+    if (!success) {
+      c.status(411);
+      return c.json({
+        message: "Inputs not correct",
+      });
+    }
 
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env.DATABASE_URL,
+      }).$extends(withAccelerate())
+  
+  
+    try {
+      const blog = await prisma.blog.create({
+        data: {
+          title: data.title,
+          content: data.content,
+        },
+      });
+  
+      return c.json({
+        "msg" : "blog is created"
+      });
+    } catch (error) {
+      c.status(500);
+      return c.json({
+        message: "Error creating blog",
+      });
+    }
+  });
